3bcfcb0a2ec41342333abf242665762cb73376bd
hudson-uchoa <uchoa.hudson1@gmail.com>
2021-12-05 23:51:52 -0300

    terminei o a cifra de césar

--- script.js
+++ script.js
@@ -24,26 +24,89 @@ var radioBase = document.querySelectorAll(".radio2");
 var btnCodDecod = document.getElementById("codDec");
 var btnCodDecod2 = document.getElementById("codDec2");
 
-radioCesar[0].addEventListener("click", function () {
+// radioCesar[0].addEventListener("click", function () {
+//   if (radioCesar[0].checked) {
+//     btnCodDecod.innerText = "Codificado\n|\n\/";
+//   }
+// });
+
+// radioCesar[1].addEventListener("click", function () {
+//   if (radioCesar[1].checked) {
+//     btnCodDecod.innerText = "Decodificar<br>|<br>\/";
+//   }
+// });
+
+// radioBase[0].addEventListener("click", function () {
+//   if (radioBase[0].checked) {
+//     btnCodDecod2.innerText = "Codificar<br>|<br>\/";
+//   }
+// });
+
+// radioBase[1].addEventListener("click", function () {
+//   if (radioBase[1].checked) {
+//     btnCodDecod2.innerText = "Decodificar<br>|<br>\/";
+//   }
+// });
+
+// Criptografia Cifra de César
+
+var entrada = document.getElementById("entrada");
+var entradaBase = document.getElementById("entrada2");
+var saida = document.getElementById("saida");
+var saida2 = document.getElementById("saida2");
+var chavePasso = document.getElementById("chavePasso");
+var valueChave
+
+entrada.addEventListener("keyup", function () {
   if (radioCesar[0].checked) {
-    btnCodDecod.innerText = "Codificar";
+    var valueText = entrada.value.split("");
+    valueChave = parseInt(chavePasso.value);
+    saida.value = codifica(valueText, valueChave);
+  } else {
+    var valueText = entrada.value.split("");
+    valueChave = parseInt(chavePasso.value);
+    saida.value = decodifica(valueText, valueChave);
   }
 });
 
-radioCesar[1].addEventListener("click", function () {
-  if (radioCesar[1].checked) {
-    btnCodDecod.innerText = "Decodificar";
-  }
-});
+function codifica(texto, chavePasso) {
+  return texto
+    .map((str) => {
+      var cripto = str.charCodeAt();
+      if (cripto >= 65 && cripto <= 90) {
+        return String.fromCharCode(((cripto - 65 + chavePasso) % 26) + 65);
+      } else if (cripto >= 97 && cripto <= 122) {
+        return String.fromCharCode(((cripto - 97 + chavePasso) % 26) + 97);
+      } else {
+        return str;
+      }
+    })
+    .join("");
+}
 
-radioBase[0].addEventListener("click", function () {
+function decodifica(texto, chavePasso) {
+  return texto
+    .map((str) => {
+      var cripto = str.charCodeAt();
+      if (cripto >= 65 && cripto <= 90) {
+        return String.fromCharCode(cripto - 65 - chavePasso < 0)?String.fromCharCode(((cripto - 65 - chavePasso +26) % 26) + 65):String.fromCharCode(((cripto -65 -chavePasso)%26)+65);
+      } else if (cripto >= 97 && cripto <= 122) {
+        return String.fromCharCode(cripto - 97 - chavePasso <0)?String.fromCharCode(((cripto - 97 - chavePasso +26) % 26) + 97):String.fromCharCode(((cripto -97 - chavePasso)%26)+65);
+      } else {
+        return str;
+      }
+    })
+    .join("");
+}
+
+// Criptografia do Base64
+
+entradaBase.addEventListener("keyup", function () {
   if (radioBase[0].checked) {
-    btnCodDecod2.innerText = "Codificar";
+  } else {
   }
 });
 
-radioBase[1].addEventListener("click", function () {
-  if (radioBase[1].checked) {
-    btnCodDecod2.innerText = "Decodificar";
-  }
-});
+function base64(input) {
+  return btoa(input);
+}
