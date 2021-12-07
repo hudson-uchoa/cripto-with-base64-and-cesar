0b86f7c6c8095135be052cec2bbc42ac9f4cc473
hudson-uchoa <uchoa.hudson1@gmail.com>
2021-12-07 18:06:02 -0300

    Codificador concluida

--- script.js
+++ script.js
@@ -26,25 +26,25 @@ var btnCodDecod2 = document.getElementById("codDec2");
 
 radioCesar[0].addEventListener("click", function () {
   if (radioCesar[0].checked) {
-    btnCodDecod.innerHTML = "Codificado";
+    btnCodDecod.innerHTML = "Resultado codificado:";
   }
 });
 
 radioCesar[1].addEventListener("click", function () {
   if (radioCesar[1].checked) {
-    btnCodDecod.innerHTML = "Decodificado";
+    btnCodDecod.innerHTML = "Resultado decodificado:";
   }
 });
 
 radioBase[0].addEventListener("click", function () {
   if (radioBase[0].checked) {
-    btnCodDecod2.innerHTML = "Codificado";
+    btnCodDecod2.innerHTML = "Resultado codificado:";
   }
 });
 
 radioBase[1].addEventListener("click", function () {
   if (radioBase[1].checked) {
-    btnCodDecod2.innerHTML = "Decodificado";
+    btnCodDecod2.innerHTML = "Resultado decodificado:";
   }
 });
 
@@ -83,22 +83,30 @@ function codifica(texto, chavePasso) {
 }
 
 function decodifica(texto, chavePasso) {
-  return texto
-    .map((str) => {
-      var cripto = str.charCodeAt();
-      if (cripto >= 65 && cripto <= 90) {
-        return String.fromCharCode(cripto - 65 - chavePasso < 0)
-          ? String.fromCharCode(((cripto - 65 - chavePasso + 26) % 26) + 65)
-          : String.fromCharCode(((cripto - 65 - chavePasso) % 26) + 65);
-      } else if (cripto >= 97 && cripto <= 122) {
-        return String.fromCharCode(cripto - 97 - chavePasso < 0)
-          ? String.fromCharCode(((cripto - 97 - chavePasso + 26) % 26) + 97)
-          : String.fromCharCode(((cripto - 97 - chavePasso) % 26) + 65);
-      } else {
-        return str;
-      }
-    })
-    .join("");
+  return (
+    texto
+      // map é um for que transforma string em array automaticamente
+      .map((str) => {
+        // famosa arrow function
+        var cripto = str.charCodeAt();
+        // limitar o ascii a letras do alfabeto maiusculas e minusculas
+        if (cripto >= 65 && cripto <= 90) {
+          // retornar em string
+          return String.fromCharCode(cripto - 65 - chavePasso < 0)
+            ? // if e else como operador condicional ternario
+              String.fromCharCode(((cripto - 65 - chavePasso + 26) % 26) + 65)
+            : String.fromCharCode(((cripto - 65 - chavePasso) % 26) + 65);
+        } else if (cripto >= 97 && cripto <= 122) {
+          return String.fromCharCode(cripto - 97 - chavePasso < 0)
+            ? String.fromCharCode(((cripto - 97 - chavePasso + 26) % 26) + 97)
+            : String.fromCharCode(((cripto - 97 - chavePasso) % 26) + 65);
+        } else {
+          return str;
+        }
+      })
+      // juntar tudo em uma string novamente
+      .join("")
+  );
 }
 
 // Criptografia do Base64
